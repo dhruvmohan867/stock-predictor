eca80775806d11b7d3f0d4d43f692a8adf83c50b
dhruvmohan867 <dhruvmohanshukla@gmail.com>
2025-10-01 11:46:45 +0530

    first commit

---
+++ data-pipeline/db_setup.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE stock_db;
+CREATE TABLE stocks (
+    id SERIAL PRIMARY KEY,
+    symbol VARCHAR(10) UNIQUE NOT NULL,
+    company_name VARCHAR(100),
+    sector VARCHAR(50)
+);
+
+CREATE TABLE stock_prices (
+    id SERIAL PRIMARY KEY,
+    stock_id INT REFERENCES stocks(id),
+    date DATE NOT NULL,
+    open NUMERIC,
+    high NUMERIC,
+    low NUMERIC,
+    close NUMERIC,
+    volume BIGINT
+);
